8e8fbcebf5774b33708513b622cff6b5457d32fa
pushkarrathi <pushkarrathi1001@gmail.com>
2026-03-23 08:34:14 +0530

    Application entry

---
+++ app/src/HotelBookingSystem.java
@@ -0,0 +1,30 @@
+/**
+ * MAIN CLASS: HotelBookingApp
+ *
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * At this stage, the application:
+ * Starts execution from the main() method
+ * Displays a welcome message to the user
+ * Confirms that the system has started successfully
+ *
+ * No business logic, data structures, or user input
+ * is implemented in this use case.
+ *
+ * The goal is to establish a clear and predictable
+ * application startup point.
+ *
+ * @author Pushkar Rathi
+ * @version 1.0
+ */
+public class HotelBookingSystem {
+    public static void main() {
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("System Initialized Successfully.");
+    }
+}
